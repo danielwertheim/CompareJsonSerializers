3268c057ae1c5fb5ec1895c3b2228382c66994ba
Daniel Wertheim <daniel@wertheim.se>
2012-01-25 12:26:40 +0100

    -Added warmup behavior and GC.Collect so that if you run all in the same process it gets more fair. - Moved to arrays

--- Source/CompareJsonSerializers/Program.cs
+++ Source/CompareJsonSerializers/Program.cs
@@ -1,81 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using CompareJsonSerializers.Model;
 
 namespace CompareJsonSerializers
 {
-	public static class f
-	{
-		public static T d<T>(fastJSON.JSON p, string j, T t)
-		{
-			return p.ToObject<T>(j);
-		}
-	}
     class Program
     {
         static void Main(string[] args)
         {
-            var numberOfCustomers = 100000;
+            var numberOfCustomers = 100;
             var numberOfItterations = 5;
 
-			//Console.WriteLine("Json.Net - Serializing");
-			//TimeSerializationAction(SerializeUsingJsonNet, numberOfCustomers, numberOfItterations);
-			//Console.WriteLine("Json.Net - Deserializing");
-			//TimeDeserializationAction(Newtonsoft.Json.JsonConvert.SerializeObject, DeSerializeUsingJsonNet, numberOfCustomers, numberOfItterations);
+			Console.WriteLine("Json.Net - Serializing");
+			TimeSerializationAction(SerializeUsingJsonNet, numberOfCustomers, numberOfItterations);
+			Console.WriteLine("Json.Net - Deserializing");
+			TimeDeserializationAction(Newtonsoft.Json.JsonConvert.SerializeObject, DeSerializeUsingJsonNet, numberOfCustomers, numberOfItterations);
 
-			//Console.WriteLine("ServiceStack.Text - Serializing");
-			//TimeSerializationAction(SerializeUsingServiceStackText, numberOfCustomers, numberOfItterations);
-			//Console.WriteLine("ServiceStack.Text - Deserializing");
-			//TimeDeserializationAction(ServiceStack.Text.JsonSerializer.SerializeToString, DeserializeUsingServiceStackText, numberOfCustomers, numberOfItterations);
+			Console.WriteLine("ServiceStack.Text - Serializing");
+			TimeSerializationAction(SerializeUsingServiceStackText, numberOfCustomers, numberOfItterations);
+			Console.WriteLine("ServiceStack.Text - Deserializing");
+			TimeDeserializationAction(ServiceStack.Text.JsonSerializer.SerializeToString, DeserializeUsingServiceStackText, numberOfCustomers, numberOfItterations);
 
-			//Console.WriteLine("fastJSon - Serializing");
-			//TimeSerializationAction(SerializeUsingFastJson, numberOfCustomers, numberOfItterations);
-			//Console.WriteLine("fastJson - Deserializing");
-			//TimeDeserializationAction(fastJSON.JSON.Instance.ToJSON, DeSerializeUsingFastJson, numberOfCustomers, numberOfItterations);
+			Console.WriteLine("fastJSon - Serializing");
+			TimeSerializationAction(SerializeUsingFastJson, numberOfCustomers, numberOfItterations);
+			Console.WriteLine("fastJson - Deserializing");
+			TimeDeserializationAction(fastJSON.JSON.Instance.ToJSON, DeSerializeUsingFastJson, numberOfCustomers, numberOfItterations);
 
             Console.ReadKey();
         }
 
-        private static void SerializeUsingJsonNet(IEnumerable<Customer> customers)
+		private static void SerializeUsingJsonNet(Customer[] customers)
         {
-            var json = customers.Select(Newtonsoft.Json.JsonConvert.SerializeObject).ToList();
+			var json = customers.Select(Newtonsoft.Json.JsonConvert.SerializeObject).ToArray();
         }
 
-        private static void DeSerializeUsingJsonNet(IEnumerable<string> json)
+		private static void DeSerializeUsingJsonNet(string[] json)
         {
-            var customers = json.Select(Newtonsoft.Json.JsonConvert.DeserializeObject<Customer>).ToList();
+			var customers = json.Select(Newtonsoft.Json.JsonConvert.DeserializeObject<Customer>).ToArray();
         }
 
-        private static void SerializeUsingServiceStackText(IEnumerable<Customer> customers)
+		private static void SerializeUsingServiceStackText(Customer[] customers)
         {
-            var json = customers.Select(ServiceStack.Text.JsonSerializer.SerializeToString).ToList();
+			var json = customers.Select(ServiceStack.Text.JsonSerializer.SerializeToString).ToArray();
         }
 
-        private static void DeserializeUsingServiceStackText(IEnumerable<string> json)
+		private static void DeserializeUsingServiceStackText(string[] json)
         {
-            var customers = json.Select(ServiceStack.Text.JsonSerializer.DeserializeFromString<Customer>).ToList();
+			var customers = json.Select(ServiceStack.Text.JsonSerializer.DeserializeFromString<Customer>).ToArray();
         }
 
-		private static void SerializeUsingFastJson(IEnumerable<Customer> customers)
+		private static void SerializeUsingFastJson(Customer[] customers)
 		{
-			var json = customers.Select(fastJSON.JSON.Instance.ToJSON).ToList();
+			var json = customers.Select(fastJSON.JSON.Instance.ToJSON).ToArray();
 		}
 
-		private static void DeSerializeUsingFastJson(IEnumerable<string> json)
+		private static void DeSerializeUsingFastJson(string[] json)
 		{
-			var customers = json.Select(fastJSON.JSON.Instance.ToObject<Customer>).ToList();
+			var customers = json.Select(fastJSON.JSON.Instance.ToObject<Customer>).ToArray();
 		}
 
-        private static void TimeSerializationAction(Action<IList<Customer>> action, int numOfCustomers, int numOfItterations)
+		private static void TimeSerializationAction(Action<Customer[]> action, int numOfCustomers, int numOfItterations)
         {
+			var customers = CustomerFactory.CreateCustomers(numOfCustomers);
+			action(customers);
+			GC.Collect();
+
             var stopWatch = new Stopwatch();
 
             for (var c = 0; c < numOfItterations; c++)
             {
-                var customers = CustomerFactory.CreateCustomers(numOfCustomers);
-
                 stopWatch.Start();
                 action(customers);
                 stopWatch.Stop();
@@ -86,14 +80,16 @@ namespace CompareJsonSerializers
             }
         }
 
-        private static void TimeDeserializationAction(Func<Customer, string> serializer, Action<IList<string>> action, int numOfCustomers, int numOfItterations)
+        private static void TimeDeserializationAction(Func<Customer, string> serializer, Action<string[]> action, int numOfCustomers, int numOfItterations)
         {
-            var stopWatch = new Stopwatch();
+			var customerJsons = CustomerFactory.CreateCustomers(numOfCustomers).Select(serializer).ToArray();
+        	action(customerJsons);
+			GC.Collect();
+            
+			var stopWatch = new Stopwatch();
 
             for (var c = 0; c < numOfItterations; c++)
             {
-                var customerJsons = CustomerFactory.CreateCustomers(numOfCustomers).Select(serializer).ToList();
-
                 stopWatch.Start();
                 action(customerJsons);
                 stopWatch.Stop();
